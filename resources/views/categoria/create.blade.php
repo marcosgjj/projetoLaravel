c0aa1a9107e43bca4130de0df1b57633c8e9683a
Wolkadan99 <marcosgjdj@gmail.com>
2022-03-22 19:50:12 -0300

    Update categoria/create

--- resources/views/categoria/create.blade.php
+++ resources/views/categoria/create.blade.php
@@ -2,10 +2,16 @@
     <x-slot name="header">
         Categorias
     </x-slot>
-    <form action="" method="post">
-        <x-label>Informe a descrição</x-label>
-        <x-input name="descricao"/>
-        <x-button>Salvar</x-button>
+    <div class="sm:ml-10 p-5 bg-white border-gray-300 max-w-7xl">
+    <form action="{{route('categoria.store')}}" method="post">
+        @csrf
+        <div>
+            <x-label>Informe a descrição:</x-label>
+            <x-input name="descricao" class="block mt-1 w-full"/>
+        </div>
+        <div class="mt-5">
+            <x-button>Salvar</x-button>
+        </div>
     </form>
-
+    </div>
 </x-app-layout>
